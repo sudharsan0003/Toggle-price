599583ac06eeec45b849b1558db99a670bf80aa4
sudharsan0003 <sanmarly04@gmail.com>
2023-06-07 11:01:00 +0530

    html and css part

---
+++ script.js
@@ -0,0 +1,21 @@
+// const button = document.getElementById("selector-box");
+// button.addEventListener("click",myPlan);
+
+// function myPlan(){
+//   var monthlyPlan =document.getElementById('monthly-price');
+//   var annualPlan =document.getElementById('annual-price');
+
+//   for(i=o; i < monthlyPlan.length; i++){
+//     for(n=0; n<annualPlan.length; n++){
+//       if(monthlyPlan[i].style.display ==="none") {
+//         monthlyPlan[i].style.display = "block";
+//         annualPayment[i].style.display = "none";
+//         button.style.justifyContent ="flex-end";
+//       }else{
+//         monthlyPlan[i].style.display = "none";
+//         annualPayment[i].style.display = "block";
+//         button.style.justifyContent ="flex-start";
+//       }
+//     }
+//   }
+// }
